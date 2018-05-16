6062e07be75cd030067c67e9a3fcfa2901c63fb8
Elvir Kuric <elvirkuric@gmail.com>
2018-05-16 08:18:43 +0200

    Signed-off-by: Elvir Kuric <elvirkuric@gmail.com>
    	modified:   files/scripts/test-mongo.sh

--- storage/mongodb/files/scripts/test-mongo.sh
+++ storage/mongodb/files/scripts/test-mongo.sh
@@ -25,15 +25,14 @@ do
   for load  in $(echo ${WORKLOAD} | sed -e s/,/" "/g);
   do
     echo "iteration: ${i} "and" ${load}"
-    mkdir -p ${output_dir}/${workload} 
     ## TODO support to override other params
     oc -n ${NAMESPACE} exec $(oc get pod -n ${NAMESPACE} | grep mongodb | awk '{print $1}') -- scl enable rh-mongodb32 -- mongo -u redhat -p redhat ${MONGODB_IP}:27017/testdb --eval "db.usertable.remove({})"
-    oc -n ${NAMESPACE} exec $(oc get pod -n ${NAMESPACE} | grep ycsb | awk '{print $1}') -- ./bin/ycsb load mongodb -s -threads "${THREADS}" -P "workloads/${load}" -p mongodb.url=mongodb://redhat:redhat@${MONGODB_IP}:27017/testdb > ${output_dir}/${workload}/mongodb_load_data_${load}_result_run_${THREADS}_${i}.txt 2>&1
+    oc -n ${NAMESPACE} exec $(oc get pod -n ${NAMESPACE} | grep ycsb | awk '{print $1}') -- ./bin/ycsb load mongodb -s -threads "${THREADS}" -P "workloads/${load}" -p mongodb.url=mongodb://redhat:redhat@${MONGODB_IP}:27017/testdb > ${output_dir}/mongodb_load_data_${load}_result_run_${THREADS}_${i}.txt 2>&1
     # run test 
-    oc -n ${NAMESPACE} exec $(oc get pod -n ${NAMESPACE} | grep ycsb | awk '{print $1}') -- ./bin/ycsb run mongodb -s -threads "${THREADS}" -P "workloads/${load}" -p mongodb.url=mongodb://redhat:redhat@${MONGODB_IP}:27017/testdb > ${output_dir}/${workload}/mongodb_run_load_${load}_result_run_${THREADS}_${i}.txt 2>&1
+    oc -n ${NAMESPACE} exec $(oc get pod -n ${NAMESPACE} | grep ycsb | awk '{print $1}') -- ./bin/ycsb run mongodb -s -threads "${THREADS}" -P "workloads/${load}" -p mongodb.url=mongodb://redhat:redhat@${MONGODB_IP}:27017/testdb > ${output_dir}/mongodb_run_load_${load}_result_run_${THREADS}_${i}.txt 2>&1
 
-    grep -E 'RunTime|Throughput' ${output_dir}/${workload}/mongodb_run_load_${load}_result_run_${THREADS}_${i}.txt > ${output_dir}/${workload}/mongodb_run_load_${load}_result_run_${THREADS}_${i}_brief.txt
-    grep Throughput ${output_dir}/${workload}/mongodb_run_load_${load}_result_run_${THREADS}_${i}.txt | cut -d',' -f3 >> ${output_dir}/${workload}/mongodb_run_load_${load}_result_run_${THREADS}_numbers.txt
+    grep -E 'RunTime|Throughput' ${output_dir}/mongodb_run_load_${load}_result_run_${THREADS}_${i}.txt > ${output_dir}/mongodb_run_load_${load}_result_run_${THREADS}_${i}_brief.txt
+    grep Throughput ${output_dir}/mongodb_run_load_${load}_result_run_${THREADS}_${i}.txt | cut -d',' -f3 >> ${output_dir}/mongodb_run_load_${load}_result_run_${THREADS}_numbers.txt
   done
 done
 
