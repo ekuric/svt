07b3f5323d3d573b8e871580d63a94c5771adb79
Elvir Kuric <elvirkuric@gmail.com>
2018-05-17 14:57:56 +0200

    Signed-off-by: Elvir Kuric <elvirkuric@gmail.com>
    added support for recordcount and operationcount for ycsb test in below files
    
    	modified:   external_vars.yaml
    	modified:   files/scripts/test-mongo.sh
    	modified:   mongodb-test.yaml
    	modified:   runmongo.sh

--- storage/mongodb/files/scripts/test-mongo.sh
+++ storage/mongodb/files/scripts/test-mongo.sh
@@ -6,6 +6,8 @@ readonly NAMESPACE=${1}
 readonly ITERATION=${2}
 readonly THREADS=${3}
 readonly WORKLOAD=${4}
+readonly RECORDCOUNT=${5}
+readonly OPERATIONCOUNT=${6}
 
 output_dir=$(dirname $0)
 
@@ -19,15 +21,18 @@ echo "NAMESPACE: ${NAMESPACE}"
 echo "ITERATION: ${ITERATION}"
 echo "THREADS: ${THREADS}"
 echo "WORKLOADS: ${WORKLOAD}"
+echo "RECORDCOUNT: ${RECORDCOUNT}"
+echo "OPERATIONCOUNT: ${OPERATIONCOUNT}"
+
 
 for i in $(seq 1 ${ITERATION}); do 
   for load  in $(echo ${WORKLOAD} | sed -e s/,/" "/g); do 
 	for thread in $(echo ${THREADS} | sed -e s/,/" "/g); do 
     		## TODO support to override other params
     		oc -n ${NAMESPACE} exec $(oc get pod -n ${NAMESPACE} | grep mongodb | awk '{print $1}') -- scl enable rh-mongodb32 -- mongo -u redhat -p redhat ${MONGODB_IP}:27017/testdb --eval "db.usertable.remove({})"
-		oc -n ${NAMESPACE} exec $(oc get pod -n ${NAMESPACE} | grep ycsb | awk '{print $1}') -- ./bin/ycsb load mongodb -s -threads $thread -P "workloads/${load}" -p mongodb.url=mongodb://redhat:redhat@${MONGODB_IP}:27017/testdb 2>&1 | tee -a ${output_dir}/mongodb_load_data_${load}_threads_${thread}_run_${i}.txt
+		oc -n ${NAMESPACE} exec $(oc get pod -n ${NAMESPACE} | grep ycsb | awk '{print $1}') -- ./bin/ycsb load mongodb -s -threads $thread -P "workloads/${load}" -p mongodb.url=mongodb://redhat:redhat@${MONGODB_IP}:27017/testdb -p recordcount=${RECORDCOUNT} -p operationcount=${OPERATIONCOUNT} 2>&1 | tee -a ${output_dir}/mongodb_load_data_${load}_threads_${thread}_run_${i}.txt
 		# run test 
-		oc -n ${NAMESPACE} exec $(oc get pod -n ${NAMESPACE} | grep ycsb | awk '{print $1}') -- ./bin/ycsb run mongodb -s -threads $thread -P "workloads/${load}" -p mongodb.url=mongodb://redhat:redhat@${MONGODB_IP}:27017/testdb 2>&1 | tee -a ${output_dir}/mongodb_run_load_${load}_threads_${thread}_run_${i}.txt
+		oc -n ${NAMESPACE} exec $(oc get pod -n ${NAMESPACE} | grep ycsb | awk '{print $1}') -- ./bin/ycsb run mongodb -s -threads $thread -P "workloads/${load}" -p mongodb.url=mongodb://redhat:redhat@${MONGODB_IP}:27017/testdb 2>&1 -p recordcount=${RECORDCOUNT} -p operationcount=${OPERATIONCOUNT}  | tee -a ${output_dir}/mongodb_run_load_${load}_threads_${thread}_run_${i}.txt
 		grep Throughput ${output_dir}/mongodb_run_load_${load}_threads_${thread}_run_${i}.txt | cut -d',' -f3 >> ${output_dir}/result_mongodb_run_load_${load}_threads_${thread}.txt
 		#grep -E 'RunTime|Throughput' ${output_dir}/mongodb_run_load_${load}_threads_${THREADS}.txt >> ${output_dir}/result_mongodb_run_load_${load}_threads_${THREADS}.txt
 
--- storage/mongodb/runmongo.sh
+++ storage/mongodb/runmongo.sh
@@ -5,7 +5,10 @@ ycsb_threads="${2}"
 JUMP_HOST="${3}"
 WORKLOAD="${4}"
 ITERATIONS="${5}"
+RECORDCOUNT="${6}"
+OPERATIONCOUNT="${7}"
 
 for memory_limit in $(echo ${MEMORY_LIMIT} | sed -e s/,/" "/g); do
-	ansible-playbook -i "${JUMP_HOST}," mongodb-test.yaml --extra-vars "MEMORY_LIMIT=${memory_limit} ycsb_threads=${ycsb_threads} workload=${WORKLOAD} iteration=${ITERATIONS}"
+	ansible-playbook -i "${JUMP_HOST}," mongodb-test.yaml --extra-vars "MEMORY_LIMIT=${memory_limit} ycsb_threads=${ycsb_threads} workload=${WORKLOAD} iteration=${ITERATIONS} \
+	recordcount=${RECORDCOUNT} operationcount=${OPERATIONCOUNT}"
 done 
