e55ed2b8d2f074dbde8ab77a40a01c9417998ed8
Elvir Kuric <elvirkuric@gmail.com>
2018-05-29 13:08:42 +0200

    Signed-off-by: Elvir Kuric <elvirkuric@gmail.com>
    	modified:   runmongo.sh
    added support for storageclass and volumesize

--- storage/mongodb/runmongo.sh
+++ storage/mongodb/runmongo.sh
@@ -7,8 +7,11 @@ WORKLOAD="${4}"
 ITERATIONS="${5}"
 RECORDCOUNT="${6}"
 OPERATIONCOUNT="${7}"
+STORAGECLASS="${8}"
+VOLUMECAPACITY="${9}"
+
 
 for memory_limit in $(echo ${MEMORY_LIMIT} | sed -e s/,/" "/g); do
 	ansible-playbook -i "${JUMP_HOST}," mongodb-test.yaml --extra-vars "MEMORY_LIMIT=${memory_limit} ycsb_threads=${ycsb_threads} workload=${WORKLOAD} iteration=${ITERATIONS} \
-	recordcount=${RECORDCOUNT} operationcount=${OPERATIONCOUNT}"
+	recordcount=${RECORDCOUNT} operationcount=${OPERATIONCOUNT} STORAGE_CLASS_NAME=${STORAGECLASS} VOLUME_CAPACITY=${VOLUMECAPACITY}" 
 done 
