49804d4ccd72ee57beb97e307546b0bc213fa7ec
Elvir Kuric <elvirkuric@gmail.com>
2018-10-26 11:02:33 +0200

    added "PBENCHCONFIG"
    Signed-off-by: Elvir Kuric <elvirkuric@gmail.com>

--- storage/mongodb/runmongo.sh
+++ storage/mongodb/runmongo.sh
@@ -9,9 +9,10 @@ RECORDCOUNT="${6}"
 OPERATIONCOUNT="${7}"
 STORAGECLASS="${8}"
 VOLUMECAPACITY="${9}"
+PBENCHCONFIG="${10}"
 
 
 for memory_limit in $(echo ${MEMORY_LIMIT} | sed -e s/,/" "/g); do
 	ansible-playbook -i "${JUMP_HOST}," mongodb-test.yaml --extra-vars "MEMORY_LIMIT=${memory_limit}Mi ycsb_threads=${ycsb_threads} workload=${WORKLOAD} iteration=${ITERATIONS} \
-	recordcount=${RECORDCOUNT} operationcount=${OPERATIONCOUNT} STORAGE_CLASS_NAME=${STORAGECLASS} VOLUME_CAPACITY=${VOLUMECAPACITY}Gi" 
+	recordcount=${RECORDCOUNT} operationcount=${OPERATIONCOUNT} STORAGE_CLASS_NAME=${STORAGECLASS} VOLUME_CAPACITY=${VOLUMECAPACITY}Gi PBENCHCONFIG=${PBENCHCONFIG}"
 done 
