96176194d9d2f32f29ed9547bc5b4d47132841b8
Elvir Kuric <elvirkuric@gmail.com>
2018-11-22 13:01:22 +0100

    get db size

--- storage/mongodb/files/scripts/test-mongo.sh
+++ storage/mongodb/files/scripts/test-mongo.sh
@@ -30,11 +30,10 @@ for i in $(seq 1 ${ITERATION}); do
 	for thread in $(echo ${THREADS} | sed -e s/,/" "/g); do 
     		oc -n ${NAMESPACE} exec $(oc get pod -n ${NAMESPACE} | grep mongodb | awk '{print $1}') -- scl enable rh-mongodb32 -- mongo -u redhat -p redhat ${MONGODB_IP}:27017/testdb --eval "db.usertable.remove({})"
 
-		# get size of database 
-
-		oc -n ${NAMESPACE} exec $(oc get pod -n ${NAMESPACE} | grep mongodb | awk '{print $1}') -- scl enable rh-mongodb32 -- mongo --eval  "db.stats(1024*1024*1024)" 127.0.0.1:27017/testdb -p redhat     -u redhat > ${output_dir}/mongodb_data_size_${load}.txt
-
 		oc -n ${NAMESPACE} exec $(oc get pod -n ${NAMESPACE} | grep ycsb | awk '{print $1}') -- ./bin/ycsb load mongodb -s -threads $thread -P "workloads/${load}" -p mongodb.url=mongodb://redhat:redhat@${MONGODB_IP}:27017/testdb -p recordcount=${RECORDCOUNT} -p operationcount=${OPERATIONCOUNT} 2>&1 | tee -a ${output_dir}/mongodb_load_data_${load}_threads_${thread}.txt
+
+		# get db size after load step 
+		oc -n ${NAMESPACE} exec $(oc get pod -n ${NAMESPACE} | grep mongodb | awk '{print $1}') -- scl enable rh-mongodb32 -- mongo --eval  "db.stats(1024*1024*1024)" 127.0.0.1:27017/testdb -p redhat         -u redhat > ${output_dir}/mongodb_data_size_${load}.txt
 
 		oc -n ${NAMESPACE} exec $(oc get pod -n ${NAMESPACE} | grep ycsb | awk '{print $1}') -- ./bin/ycsb run mongodb -s -threads $thread -P "workloads/${load}" -p mongodb.url=mongodb://redhat:redhat@${MONGODB_IP}:27017/testdb 2>&1 -p recordcount=${RECORDCOUNT} -p operationcount=${OPERATIONCOUNT}  | tee -a ${output_dir}/mongodb_run_load_${load}_threads_${thread}.txt
 	done 
