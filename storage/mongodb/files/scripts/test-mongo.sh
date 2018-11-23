e33912048ca57354403e8d987add82e80bbe044f
Elvir Kuric <elvirkuric@gmail.com>
2018-11-23 09:53:40 +0100

    dropdatabase after every iteration

--- storage/mongodb/files/scripts/test-mongo.sh
+++ storage/mongodb/files/scripts/test-mongo.sh
@@ -24,20 +24,21 @@ echo "WORKLOADS: ${WORKLOAD}"
 echo "RECORDCOUNT: ${RECORDCOUNT}"
 echo "OPERATIONCOUNT: ${OPERATIONCOUNT}"
 
+# load phase 
 
 for i in $(seq 1 ${ITERATION}); do 
-  for load  in $(echo ${WORKLOAD} | sed -e s/,/" "/g); do 
-	for thread in $(echo ${THREADS} | sed -e s/,/" "/g); do 
-    		oc -n ${NAMESPACE} exec $(oc get pod -n ${NAMESPACE} | grep mongodb | awk '{print $1}') -- scl enable rh-mongodb32 -- mongo -u redhat -p redhat ${MONGODB_IP}:27017/testdb --eval "db.usertable.remove({})"
-
-		oc -n ${NAMESPACE} exec $(oc get pod -n ${NAMESPACE} | grep ycsb | awk '{print $1}') -- ./bin/ycsb load mongodb -s -threads $thread -P "workloads/${load}" -p mongodb.url=mongodb://redhat:redhat@${MONGODB_IP}:27017/testdb -p recordcount=${RECORDCOUNT} -p operationcount=${OPERATIONCOUNT} 2>&1 | tee -a ${output_dir}/mongodb_load_data_${load}_threads_${thread}.txt
+	ADMIN_PASS=$(oc -n ${NAMESPACE} exec $(oc get pod -n ${NAMESPACE} | grep mongodb | awk '{print $1}') -- scl enable rh-mongodb32 env |  env | grep MONGODB_ADMIN_PASSWORD | cut -d'=' -f2)
+	oc -n ${NAMESPACE} exec $(oc get pod -n ${NAMESPACE} | grep mongodb | awk '{print $1}') -- scl enable rh-mongodb32 -- mongo testdb -p ${ADMIN_PASS} -u admin --authenticationDatabase "admin" --eval "db.dropDatabase()" 
+	for load  in $(echo ${WORKLOAD} | sed -e s/,/" "/g); do 
+		for thread in $(echo ${THREADS} | sed -e s/,/" "/g); do 
+			oc -n ${NAMESPACE} exec $(oc get pod -n ${NAMESPACE} | grep ycsb | awk '{print $1}') -- ./bin/ycsb load mongodb -s -threads $thread -P "workloads/${load}" -p mongodb.url=mongodb://redhat:redhat@${MONGODB_IP}:27017/testdb -p recordcount=${RECORDCOUNT} -p operationcount=${OPERATIONCOUNT} 2>&1 | tee -a ${output_dir}/mongodb_load_data_${load}_threads_${thread}.txt
 
 		# get db size after load step 
-		oc -n ${NAMESPACE} exec $(oc get pod -n ${NAMESPACE} | grep mongodb | awk '{print $1}') -- scl enable rh-mongodb32 -- mongo --eval  "db.stats(1024*1024*1024)" 127.0.0.1:27017/testdb -p redhat         -u redhat > ${output_dir}/mongodb_data_size_${load}.txt
+			oc -n ${NAMESPACE} exec $(oc get pod -n ${NAMESPACE} | grep mongodb | awk '{print $1}') -- scl enable rh-mongodb32 -- mongo --eval  "db.stats(1024*1024*1024)" 127.0.0.1:27017/testdb -p redhat         -u redhat > ${output_dir}/mongodb_data_size_${load}.txt
 
-		oc -n ${NAMESPACE} exec $(oc get pod -n ${NAMESPACE} | grep ycsb | awk '{print $1}') -- ./bin/ycsb run mongodb -s -threads $thread -P "workloads/${load}" -p mongodb.url=mongodb://redhat:redhat@${MONGODB_IP}:27017/testdb 2>&1 -p recordcount=${RECORDCOUNT} -p operationcount=${OPERATIONCOUNT}  | tee -a ${output_dir}/mongodb_run_load_${load}_threads_${thread}.txt
-	done 
-   done 
+			oc -n ${NAMESPACE} exec $(oc get pod -n ${NAMESPACE} | grep ycsb | awk '{print $1}') -- ./bin/ycsb run mongodb -s -threads $thread -P "workloads/${load}" -p mongodb.url=mongodb://redhat:redhat@${MONGODB_IP}:27017/testdb 2>&1 -p recordcount=${RECORDCOUNT} -p operationcount=${OPERATIONCOUNT}  | tee -a ${output_dir}/mongodb_run_load_${load}_threads_${thread}.txt
+		done 
+   	done 
 done
 
 # sort result 
